692ae41c0bae45319215dabeaf5182e47a3da757
alex <alex.suspicious@gmail.com>
2024-11-08 14:49:41 +0500

    index.js changes, some packages were removed or changed due to large output file size

--- src/index.js
+++ src/index.js
@@ -1,53 +1,51 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('node:path');
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
+const { app, protocol, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  protocol.interceptFileProtocol('file', (request, callback) => {
+    const parsedUrl = url.parse(request.url); // Parse the URL
+    const pathname = decodeURIComponent(parsedUrl.pathname.substr(1)); // Strip off the leading '/' and decode URL components
+    const query = parsedUrl.query; // Extract the query parameters if needed
+
+    // Here you can handle the query parameters (if any)
+    console.log('Query parameters:', query); // Example: log the query
+
+    // Resolve the file path
+    const normalizedPath = path.normalize(`${__dirname}/${pathname}`);
+
+    callback({ path: normalizedPath });
+  });
+
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    frame: true,
+    icon: path.join(__dirname, 'favicon.ico'),
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: false,
-      experimentalFeatures: true
-    },
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.removeMenu()
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
-
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+      nodeIntegration: false, // Enable Node.js integration if necessary
+      contextIsolation: false, // Disable context isolation to use Node.js in the renderer process
+      webSecurity: false
     }
-  });
-});
+  })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+  win.removeMenu();
+  //win.loadFile('/index.html');
+
+  //win.loadURL(`file:///index.html?dark=1`);
+  //win.loadURL(path.normalize(`/index.html`));
+  //win.loadFile('/index.html');
+
+  win.loadURL('file:///index.html');
+  //win.loadURL(`file://${__dirname}/index.html`)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+
+app.on("browser-window-created", (e, win) => {
+    win.removeMenu();
+});
